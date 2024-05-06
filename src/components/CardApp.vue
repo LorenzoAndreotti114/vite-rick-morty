1610a5a819907eb79dcf1ea8a50e4169f0c94501
LorenzoAndreotti114 <lorenzoandr123@gmail.com>
2024-05-07 01:21:02 +0200

    final project

---
+++ src/components/CardApp.vue
@@ -0,0 +1,91 @@
+<script>
+    import axios from 'axios';
+
+    export default {
+        data() {
+            return {
+                cardsData: []
+            }
+        },
+        created() {
+            axios
+                .get("https://rickandmortyapi.com/api/character")
+                .then((resp) => {
+                    console.log(resp);
+                    this.cardsData = resp.data.results
+                    console.log(this.cardsData);
+                })
+        }
+    }
+
+
+</script>
+
+<template>
+    <div class="card-container">
+
+        <div class="card" v-for="(CardCreator, index) in cardsData">
+            <div class="card-image">
+                <img :src="cardsData[index].image" alt="">
+            </div>
+
+            <div class="card-title f-size">
+                <div class="card-name">
+                    <h2>
+                        {{ cardsData[index].name }}
+                    </h2>
+                </div>
+
+                <div>
+                    {{ cardsData[index].status }}
+                </div>
+
+                <div>
+                    {{ cardsData[index].species }}
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+
+    img {
+        border-radius: 50%;
+    }
+
+    .card {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .card-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        width: 100%;
+        padding: 5%;
+    }
+
+    .card-image {
+        height: 20vw;
+        width: 20vw;
+        margin: 10% 0;
+    }
+
+    .card-title {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+    }
+
+    .card-name {
+        font-size: 1.2vw;
+        width: 100%;
+    }
+</style>
---
+++ src/components/MainApp.vue
@@ -0,0 +1,42 @@
+<script>
+
+    import CardApp from './CardApp.vue'
+
+    export default {
+        data() {
+            return {
+
+            }
+        },
+
+        components: {
+            CardApp,
+        }
+    }
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="title">
+            <h1>Rick And Morty App</h1>
+        </div>
+    </div>
+
+    <CardApp />
+</template>
+
+<style scoped>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 10vh;
+        width: 100%;
+        padding: 5%;
+    }
+
+    .title {
+        font-size: 1.5vw;
+    }
+</style>
